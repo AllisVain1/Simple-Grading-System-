d5e3e16ed48d109cd6492f6dc569d84b807346e9
AllisVain1 <ejikewonderful@gmail.com>
2025-07-10 20:21:02 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,26 @@
+#Declaring mark range and Score
+info = "Ejike Wonderful.Aguchikwu"
+print(info)
+int(input"Enter your score")
+
+    if 70 <= score <= 100:
+        return 'A'
+    elif 60 <= score <= 69:
+        return 'B'
+    elif 50 <= score <= 59:
+        return 'C'
+    elif 45 <= score <= 49:
+        return 'D'
+    elif 40 <= score <= 44:
+        return 'E'
+    elif 0 <= score <= 39:
+        return 'F'
+    else:
+        return 'Invalid Score'
+#Create user message
+print("Results assigned.")
+
+
+
+
+
